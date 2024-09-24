feffe6a6aff1a3f8521e033a8fb6d609b7ae6f12
Saru <heo_dark@naver.com>
2024-09-25 02:31:54 +0900

    feat: 포인트 사용 기능 및 테스트 추가

--- src/main/java/io/hhplus/tdd/point/service/impl/DefaultPointService.java
+++ src/main/java/io/hhplus/tdd/point/service/impl/DefaultPointService.java
@@ -16,13 +16,31 @@ public class DefaultPointService implements PointService {
     private final DefaultPointHistoryRepository defaultPointHistoryRepository;
 
     @Override
-    public UserPoint charge(Long id, long amount) {
+    public UserPoint charge(long id, long amount) {
         if (0 >= amount) {
-            throw new IllegalArgumentException("0원 이상 충전 되어야 합니다.");
+            throw new IllegalArgumentException("포인트는 0원 이상 충전 되어야 합니다.");
         }
 
-        long currentAmount = defaultUserPointRepository.selectById(id).point();
-        long updateAmount = amount + currentAmount;
+        long updateAmount =  defaultUserPointRepository.selectById(id).chargePoint(amount);
+
+        UserPoint updateUserPoint = defaultUserPointRepository.insertOrUpdate(id, updateAmount);
+        defaultPointHistoryRepository.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updateUserPoint;
+    }
+
+    @Override
+    public UserPoint use(long id, long amount) {
+
+        if (amount > 5000) {
+            throw new IllegalArgumentException("포인트는 5000원 이상 사용 불가합니다.");
+        }
+
+        long updateAmount = defaultUserPointRepository.selectById(id).usePoint(amount);
+
+        if (0 > updateAmount) {
+            throw new IllegalArgumentException("포인트는 잔고이상 사용할 수 없습니다.");
+        }
 
         UserPoint updateUserPoint = defaultUserPointRepository.insertOrUpdate(id, updateAmount);
         defaultPointHistoryRepository.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());

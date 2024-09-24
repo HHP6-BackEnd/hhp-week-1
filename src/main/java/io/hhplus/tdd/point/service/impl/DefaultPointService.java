2a95708474c54b7299b8f406264f90379de7eba9
Saru <heo_dark@naver.com>
2024-09-25 00:15:30 +0900

    feat: charge 기능 추가

--- src/main/java/io/hhplus/tdd/point/service/impl/DefaultPointService.java
+++ src/main/java/io/hhplus/tdd/point/service/impl/DefaultPointService.java
@@ -1,4 +1,32 @@
 package io.hhplus.tdd.point.service.impl;
 
-public class DefaultPointService {
+import io.hhplus.tdd.point.TransactionType;
+import io.hhplus.tdd.point.UserPoint;
+import io.hhplus.tdd.point.domain.repository.impl.DefaultPointHistoryRepository;
+import io.hhplus.tdd.point.domain.repository.impl.DefaultUserPointRepository;
+import io.hhplus.tdd.point.service.PointService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DefaultPointService implements PointService {
+
+    private final DefaultUserPointRepository defaultUserPointRepository;
+    private final DefaultPointHistoryRepository defaultPointHistoryRepository;
+
+    @Override
+    public UserPoint charge(Long id, long amount) {
+        if (0 >= amount) {
+            throw new IllegalArgumentException("0원 이상 충전 되어야 합니다.");
+        }
+
+        long currentAmount = defaultUserPointRepository.selectById(id).point();
+        long updateAmount = amount + currentAmount;
+
+        UserPoint updateUserPoint = defaultUserPointRepository.insertOrUpdate(id, updateAmount);
+        defaultPointHistoryRepository.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+        return updateUserPoint;
+    }
 }

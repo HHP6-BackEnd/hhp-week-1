38dce6442efdd5d7ed3c4345793340418969e6c3
Saru <heo_dark@naver.com>
2024-09-25 23:42:29 +0900

    test: charge, use 서비스 단위테스트 작성

--- src/test/java/io/hhplus/tdd/point/service/impl/DefaultPointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/service/impl/DefaultPointServiceTest.java
@@ -1,15 +1,18 @@
 package io.hhplus.tdd.point.service.impl;
 
+import io.hhplus.tdd.point.PointHistory;
 import io.hhplus.tdd.point.TransactionType;
 import io.hhplus.tdd.point.UserPoint;
 import io.hhplus.tdd.point.domain.repository.impl.DefaultPointHistoryRepository;
 import io.hhplus.tdd.point.domain.repository.impl.DefaultUserPointRepository;
+import io.hhplus.tdd.point.service.PointValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.BDDMockito.*;
@@ -18,6 +21,9 @@ import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class DefaultPointServiceTest {
+
+    @Spy
+    private PointValidator pointValidator;
 
     @Mock
     private DefaultUserPointRepository defaultUserPointRepository;
@@ -36,100 +42,64 @@ class DefaultPointServiceTest {
     }
 
     @Test
-    @DisplayName("해당 회원의 요청한 만큼의 포인트 충전 후 저장")
+    @DisplayName("id가 1인 회원이 500 포인트 충전 요청시 충전 성공 후 1500포인트 반환")
     void testCharge() {
 
         // given
-        long currentPoint = userPoint.point();
-        long amount = 500L;
-        long updatePoint = currentPoint + amount;
 
-        // 1L 유저 조회시 1000L 마일리지 조회 되도록 주입
-        given(defaultUserPointRepository.selectById(1L)).willReturn(userPoint);
-        // 1L 유저 포인트 업데이트 시 updatePoint 로 저장 되도록 주입
-        given(defaultUserPointRepository.insertOrUpdate(1L, updatePoint)).willReturn(new UserPoint(1L, updatePoint, System.currentTimeMillis()));
+        // 유저포인트 조회 시 1000 으로 조회 되도록 주입
+        given(defaultUserPointRepository.selectById(anyLong())).willReturn(userPoint);
+        // 마지막 포인트 저장시 1500 포인트 반환 되도록 주입
+        given(defaultUserPointRepository.insertOrUpdate(anyLong(), anyLong())).willReturn(new UserPoint(1L, 1500L, System.currentTimeMillis()));
 
         // when
-        // 1L 유저 포인트 500충전
-        UserPoint resultUserPoint = defaultPointService.charge(1L, amount, TransactionType.CHARGE);
+
+        // 포인트 500충전
+        UserPoint resultUserPoint = defaultPointService.charge(1L, 500L, TransactionType.CHARGE);
 
         // then
-        // 1000 + 500 = 1500 포인트인지 확인
+
         assertThat(resultUserPoint.point()).isEqualTo(1500L);
+        // 충전 amount 검증 호출 확인
+        then(pointValidator).should(times(1)).verifyChargeAmount(anyLong());
+        // 유저 포인트 조회 호출 확인
         then(defaultUserPointRepository).should(times(1)).selectById(1L);
-        then(defaultUserPointRepository).should(times(1)).insertOrUpdate(1L, updatePoint);
+        // 충전 point 검증 호출 확인
+        then(pointValidator).should(times(1)).verifyChargePoint(anyLong());
+        // 최종 업데이트 호출 확인
+        then(defaultUserPointRepository).should(times(1)).insertOrUpdate(1L, 1500L);
+        // 포인트 히스토리 저장 호출 확인
+        then(defaultPointHistoryRepository).should(times(1)).insert(anyLong(), anyLong(), any(TransactionType.class), anyLong());
 
     }
 
     @Test
-    @DisplayName("0원 이하의 포인트 충전 요청시 에러 발생")
-    void testAmountUnderZeroCharge() {
-
-        // given
-        long amount = 0;
-
-        // when
-        // 1L 유저 포인트 500충전
-        assertThatThrownBy(() -> defaultPointService.charge(1L, amount, TransactionType.CHARGE))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("0원 이상 충전 되어야 합니다.");
-
-    }
-
-    @Test
-    @DisplayName("해당 회원의 요청한 만큼의 포인트를 사용 한 후 차감")
+    @DisplayName("id가 1인 회원이 500 포인트 사용 요청시 사용 성공 후 500포인트 반환")
     void testUse() {
 
         // given
-        long currentPoint = userPoint.point();
-        long amount = 500L;
-        long updatePoint = currentPoint - amount;
 
-        // 1L 유저 조회시 1000L 마일리지 조회 되도록 주입
-        given(defaultUserPointRepository.selectById(1L)).willReturn(userPoint);
-        // 1L 유저 포인트 업데이트 시 updatePoint 로 저장 되도록 주입
-        given(defaultUserPointRepository.insertOrUpdate(1L, updatePoint)).willReturn(new UserPoint(1L, updatePoint, System.currentTimeMillis()));
+        // 유저포인트 조회 시 1000 으로 조회 되도록 주입
+        given(defaultUserPointRepository.selectById(anyLong())).willReturn(userPoint);
+        // 마지막 포인트 저장시 500 포인트 반환 되도록 주입
+        given(defaultUserPointRepository.insertOrUpdate(anyLong(), anyLong())).willReturn(new UserPoint(1L, 500L, System.currentTimeMillis()));
 
         // when
-        // 1L 유저 포인트 500사용
-        UserPoint resultUserPoint = defaultPointService.use(1L, amount, TransactionType.USE);
+        // 포인트 500사용
+        UserPoint resultUserPoint = defaultPointService.use(1L, 500L, TransactionType.USE);
 
         // then
-        // 1000 + 500 = 1500 포인트인지 확인
         assertThat(resultUserPoint.point()).isEqualTo(500L);
+        // 사용 amount 검증 호출 확인
+        then(pointValidator).should(times(1)).verifyUseAmount(anyLong());
+        // 유저 포인트 조회 호출 확인
         then(defaultUserPointRepository).should(times(1)).selectById(1L);
-        then(defaultUserPointRepository).should(times(1)).insertOrUpdate(1L, updatePoint);
-    }
-
-    @Test
-    @DisplayName("5000원 이상 포인트 사용시 에러발생")
-    void testAmountOverFiveKUse() {
-
-        // given
-        long amount = 5001;
-
-        // when
-        // 1L 유저 포인트 5001 사용
-        assertThatThrownBy(() -> defaultPointService.use(1L, amount, TransactionType.USE))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("포인트는 5000원 이상 사용 불가합니다.");
-
-    }
-
-    @Test
-    @DisplayName("0포인트 이하 포인트 사용시 에러발생")
-    void testAmountUnderZeroUse() {
-
-        // given
-        long amount = 1001;
-        // 1L 유저 조회시 1000L 마일리지 조회 되도록 주입
-        given(defaultUserPointRepository.selectById(1L)).willReturn(userPoint);
-
-        // when
-        // 1L 유저 포인트 1001 사용
-        assertThatThrownBy(() -> defaultPointService.use(1L, amount, TransactionType.USE))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("포인트는 잔고이상 사용할 수 없습니다.");
+        // 사용 point 검증 호출 확인
+        then(pointValidator).should(times(1)).verifyUsePoint(anyLong());
+        // 최종 업데이트 호출 확인
+        then(defaultUserPointRepository).should(times(1)).insertOrUpdate(1L, 500L);
+        // 포인트 히스토리 저장 호출 확인
+        then(defaultPointHistoryRepository).should(times(1)).insert(anyLong(), anyLong(), any(TransactionType.class), anyLong());
 
     }
 
